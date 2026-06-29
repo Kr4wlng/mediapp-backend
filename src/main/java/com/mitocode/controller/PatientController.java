44d78f05f5b4c282de0cea2aa513bac4805855f8
adurand <adurand@sintad.pe>
2026-06-29 14:54:30 -0500

    TypeMap en ModelMapper

--- src/main/java/com/mitocode/controller/PatientController.java
+++ src/main/java/com/mitocode/controller/PatientController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,6 +21,7 @@ import java.util.List;
 public class PatientController {
 
     private final IPatientService service;
+    @Qualifier("defaultMapper")
     private final ModelMapper modelMapper;
 
     @GetMapping
